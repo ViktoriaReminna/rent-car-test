9a7d0c5b700ecc80dc2c6395b9379f6857b56177
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2024-02-03 12:30:21 +0200

    add img

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import { Wrapper, Title, Text } from './Home.styled';
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <div>
+        <Title>
+          Welcome to Rent Club - Your Gateway to Seamless Travel Experiences!
+        </Title>
+        <Text>
+          Whether you're embarking on a business trip, a family vacation, or a
+          spontaneous road adventure, we have the perfect vehicle to suit your
+          needs.
+        </Text>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Home;
